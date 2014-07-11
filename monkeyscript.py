bfe3d72a89f017781555a954c5dd4b7bb166a8d8
x7 <x7.0@outlook.com>
2014-07-11 11:55:49 +0800

    start activity using scheme

--- monkeyscript.py
+++ monkeyscript.py
@@ -4,15 +4,10 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 # Connects to the current device, returning a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 
-# sets a variable with the package's internal name
-package = 'com.sina.weibo'
-
-# sets a variable with the name of an Activity in the package
-#mainActivity = 'com.sina.weibo.MainTabActivity'
-#cardActivity = 'com.sina.weibo.CardListActivity'
-
 #device.startActivity(component=package + '/' + mainActivity)
-#MonkeyRunner.sleep(3)
-#device.startActivity(component=package + '/' + cardActivity)
+
+device.startActivity(uri='sinaweibo://cardlist?containerid=102803&cache_need=1&count=10')
+
+MonkeyRunner.sleep(10)
 
 device.drag((250,850),(250,110),0.1,10)
