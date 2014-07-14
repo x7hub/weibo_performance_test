44b55d636a754b57cdf40029056b33a153fd4c45
x7 <x7.0@outlook.com>
2014-07-14 14:53:29 +0800

    test topic|hot|find|music|home

--- card_performance_test.sh
+++ card_performance_test.sh
@@ -1,21 +1,87 @@
 #! /bin/sh
 # card_performance_test.sh
 
-TARGET_PKG='com.sina.weibo'
+# -f    fast scroll
+# -p    target page
+# -t    test another app
+
+usage()
+{
+    echo "Usage: `basename $0` [-f] [-p topic|hot|find|music|home] [-t google|tweet|fb]"
+    exit 1
+}
+
+[ $# -eq 0 ] && usage
 
 DIR_NOW=`date +%s` # use as sub dir name
 DIR_RESULT='result'
+
+target_pkg='com.sina.weibo'
+sleep_time=1;
 
 # store test output
 mkdir -p ${DIR_RESULT}/${DIR_NOW}
 cd ${DIR_RESULT}/${DIR_NOW}
 
-# first arg as conainner id
-if [ $1 ]
+# first arg
+while getopts fp:t: opt
+do
+    case $opt in
+        p)
+            case $OPTARG in
+                'topic')
+                    echo 'OPTARG topic'
+                    scheme='pageinfo?containerid=100808db057fb6820d53fee904474bcbb75b1a'
+                    ;;
+                'hot' )
+                    echo 'OPTARG hot'
+                    scheme='cardlist?containerid=100803'
+                    ;;
+                'find')
+                    echo 'OPTARG find'
+                    scheme='cardlist?containerid=1087030002_417'
+                    ;;
+                'music')
+                    echo 'OPTARG music'
+                    scheme='cardlist?containerid=10140310001'
+                    ;;
+                'home')
+                    echo 'OPTARG home'
+                    scheme='gotohome'
+                    ;;
+                \?)
+                    echo -e 'invalid arg'
+                    exit 1
+                    ;;
+            esac
+            ;;
+        f)
+            sleep_time=0.5
+            ;;
+        t)
+            case $OPTARG in
+                'google')
+                    echo "not available now"
+                    ;;
+                'tweet')
+                    echo "not available now"
+                    ;;
+                'fb')
+                    echo "not available now"
+                    ;;
+            esac
+            ;;
+        *)
+            echo -e 'invalid arg'
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $scheme ]
 then
-    containerid=$1
-else
-    containerid=102803
+    echo 'need arg -p'
+    exit 1
 fi
 
 # funcion for drawing histogram with gfxinfo
@@ -45,8 +111,8 @@ function read_gfx_info()
     for i in `seq 3`
     do
         echo '******* read gfxinfo *******'
-        adb shell dumpsys gfxinfo ${TARGET_PKG} | sed -n '/Draw\tProcess\tExecute/,/View hierarchy:/p'| tee gfxinfo${i}.cvs | grep [0-9] | awk '{printf("%02d %s\n", NR, $0)}' >> gfxinfoSum.cvs
-        sleep 7
+        adb shell dumpsys gfxinfo ${target_pkg} | sed -n '/Draw\tProcess\tExecute/,/View hierarchy:/p'| tee gfxinfo${i}.cvs | grep [0-9] | awk '{printf("%02d %s\n", NR, $0)}' >> gfxinfoSum.cvs
+        sleep `echo 6*${sleep_time}+2 | bc`
     done
     draw_histogram
 }
@@ -57,25 +123,40 @@ function run_monkey_script()
     cat << EOF | monkeyrunner
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 device = MonkeyRunner.waitForConnection()
-device.startActivity(uri='sinaweibo://cardlist?containerid=${containerid}')
-MonkeyRunner.sleep(5)
+device.startActivity(uri='sinaweibo://${scheme}')
+MonkeyRunner.sleep(10)
 for i in range(0,3):
-    device.drag((216,768),(216,153),0.1,10)
-    MonkeyRunner.sleep(1)
-    device.drag((216,768),(216,153),0.1,10)
-    MonkeyRunner.sleep(1)
-    device.drag((216,153),(216,768),0.1,10)
-    MonkeyRunner.sleep(1)
-    device.drag((216,768),(216,153),0.1,10)
-    MonkeyRunner.sleep(1)
-    device.drag((216,153),(216,768),0.1,10)
-    MonkeyRunner.sleep(1)
-    device.drag((216,153),(216,768),0.1,10)
-    MonkeyRunner.sleep(1)
+    device.drag((216,768),(216,153),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
+    device.drag((216,768),(216,153),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
+    device.drag((216,153),(216,768),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
+    device.drag((216,768),(216,153),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
+    device.drag((216,153),(216,768),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
+    device.drag((216,153),(216,768),0.08,10)
+    MonkeyRunner.sleep(${sleep_time})
 
 EOF
 }
 
 run_monkey_script &
-sleep 15
+sleep 20
 read_gfx_info
+
+if read -n 1 -p "Save ? [Y/n]:"
+then  
+    case $REPLY in  
+        N|n)
+            echo -e "\nexit.\n"  
+            exit
+            ;;
+        *)
+            echo '\n'
+            read -p "Save as :" saveas
+            cp -i gfxinfo.png ../${saveas}.png
+            ;;  
+    esac   
+fi   
